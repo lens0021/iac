8fb1d2ada309ebe971bd5b944255313d3e6df600
lens0021 <lorentz0021@gmail.com>
2023-06-17 21:23:21 +0900

    Rename

--- oci_core.tf
+++ oci_core.tf
@@ -2,32 +2,32 @@ data "oci_identity_compartment" "root" {
   id = "ocid1.tenancy.oc1..aaaaaaaamsyx65yzn6dlrfg2moqhmfhdb3bhipgwtb326vhw62xtb4jmu5za"
 }
 
-resource "oci_identity_compartment" "default" {
+resource "oci_identity_compartment" "blue" {
   compartment_id = data.oci_identity_compartment.root.id
-  description    = "A default compartment"
-  name           = "default"
+  description    = "A compartment"
+  name           = "blue"
 }
 
-data "oci_identity_availability_domain" "default" {
+data "oci_identity_availability_domain" "ad1" {
   compartment_id = data.oci_identity_compartment.root.id
   ad_number      = 1
 }
 
-resource "oci_core_vcn" "default" {
+resource "oci_core_vcn" "blue" {
   cidr_block     = "10.1.0.0/16"
-  compartment_id = oci_identity_compartment.default.id
+  compartment_id = oci_identity_compartment.blue.id
 }
 
-resource "oci_core_subnet" "default" {
-  availability_domain = data.oci_identity_availability_domain.default.id
+resource "oci_core_subnet" "blue" {
+  availability_domain = data.oci_identity_availability_domain.ad1.id
   cidr_block          = "10.1.20.0/24"
-  compartment_id      = oci_identity_compartment.default.id
-  vcn_id              = oci_core_vcn.default.id
+  compartment_id      = oci_identity_compartment.blue.id
+  vcn_id              = oci_core_vcn.blue.id
 }
 
 resource "oci_core_instance" "blue" {
-  compartment_id      = oci_identity_compartment.default.id
-  availability_domain = data.oci_identity_availability_domain.default.id
+  compartment_id      = oci_identity_compartment.blue.id
+  availability_domain = data.oci_identity_availability_domain.ad1.id
   shape               = "VM.Standard.A1.Flex"
 
   source_details {
@@ -36,7 +36,7 @@ resource "oci_core_instance" "blue" {
   }
 
   create_vnic_details {
-    subnet_id = oci_core_subnet.default.id
+    subnet_id = oci_core_subnet.blue.id
   }
 
   lifecycle {
@@ -47,7 +47,7 @@ resource "oci_core_instance" "blue" {
 }
 
 data "oci_core_images" "ubuntu_minimal" {
-  compartment_id = oci_identity_compartment.default.id
+  compartment_id = oci_identity_compartment.blue.id
 
   operating_system = "Canonical Ubuntu"
   shape            = "VM.Standard.A1.Flex"
