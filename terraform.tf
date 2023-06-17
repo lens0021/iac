dcd11ccb1f9a4e00d9c38116c2f59c7424e06aa9
lens0021 <lorentz0021@gmail.com>
2023-06-17 21:13:11 +0900

    Bump oci provider

--- terraform.tf
+++ terraform.tf
@@ -14,6 +14,13 @@ variable "oci_region" {
 terraform {
   required_version = "~> 1.0"
 
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 5.0"
+    }
+  }
+
   backend "remote" {
     organization = "lens0021"
 
