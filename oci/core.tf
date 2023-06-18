d85663ceb13deef3c00747ad08f025bcc3cc02b1
lens0021 <lorentz0021@gmail.com>
2023-06-18 16:13:50 +0900

    Use the default_route_table

--- oci/core.tf
+++ oci/core.tf
@@ -33,21 +33,9 @@ resource "oci_core_internet_gateway" "blue" {
   vcn_id         = oci_core_vcn.blue.id
 }
 
-resource "oci_core_route_table" "blue" {
-  compartment_id = oci_identity_compartment.blue.id
-  vcn_id         = oci_core_vcn.blue.id
-
-  route_rules {
-    network_entity_id = oci_core_internet_gateway.blue.id
-
-    destination      = "0.0.0.0/0"
-    destination_type = "CIDR_BLOCK"
-  }
-}
-
 resource "oci_core_route_table_attachment" "blue" {
   subnet_id      = oci_core_subnet.blue.id
-  route_table_id = oci_core_route_table.blue.id
+  route_table_id = oci_core_vcn.blue.default_route_table_id
 }
 
 resource "oci_core_instance" "blue" {
