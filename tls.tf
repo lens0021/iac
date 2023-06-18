157c8910322f7f3f3d59b4f93887eb5c5cf5f201
lens0021 <lorentz0021@gmail.com>
2023-06-18 03:40:33 +0000

    Update tls.tf
    The default value is not supported

--- tls.tf
+++ tls.tf
@@ -1,4 +1,5 @@
 # Do not mimic this. Use of this resource for production deployments is not recommended
 resource "tls_private_key" "oci" {
-  algorithm = "ECDSA"
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
 }
