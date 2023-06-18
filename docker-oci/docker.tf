07379b0b18dc416c830315f519afb41ed629d1b9
lens0021 <lorentz0021@gmail.com>
2023-06-18 15:47:53 +0900

    Move provider to terraform.tf back

--- docker-oci/docker.tf
+++ docker-oci/docker.tf
@@ -1,9 +1,3 @@
-provider "docker" {
-  # TODO
-  # host     = "ssh://ubuntu@${oci_core_instance.blue.public_ip}:22"
-  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
-}
-
 resource "docker_image" "hello_world" {
   name = "hello-world"
 }
--- docker-oci/terraform.tf
+++ docker-oci/terraform.tf
@@ -16,3 +16,9 @@ terraform {
     }
   }
 }
+
+provider "docker" {
+  # TODO
+  # host     = "ssh://ubuntu@${oci_core_instance.blue.public_ip}:22"
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+}
