126cf54cdfc5aae61ae07c8803b686f76cb29d2d
lens0021 <lorentz0021@gmail.com>
2023-06-18 17:04:16 +0900

    Improve user_data

--- oci/core_instance_user_data.sh
+++ oci/core_instance_user_data.sh
@@ -2,21 +2,23 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+apt-get update
+apt-get install -y less vim
+
 # Install and configure Docker
 # https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
-sudo apt-get update
-sudo apt-get install -y
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+apt-get install -y ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+  tee /etc/apt/sources.list.d/docker.list >/dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
-sudo usermod -a -G docker ubuntu
+usermod -a -G docker ubuntu
 
 echo Hello, OCI
