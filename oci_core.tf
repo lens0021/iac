c1d30658b57130e09b606e3868b20750ebce726a
lens0021 <lorentz0021@gmail.com>
2023-06-18 11:00:44 +0900

    availability_domain should be name

--- oci_core.tf
+++ oci_core.tf
@@ -28,7 +28,7 @@ resource "oci_core_instance" "blue" {
   async = false
 
   compartment_id      = oci_identity_compartment.blue.id
-  availability_domain = data.oci_identity_availability_domain.ad1.id
+  availability_domain = data.oci_identity_availability_domain.ad1.name
   shape               = data.oci_core_images.ubuntu_minimal.shape
 
   shape_config {
@@ -54,67 +54,6 @@ resource "oci_core_instance" "blue" {
     ignore_changes = [
       source_details,
     ]
-  }
-}
-
-resource "oci_core_instance" "green" {
-  async = false
-
-  availability_domain = data.oci_identity_availability_domain.ad1.id
-
-  compartment_id = oci_identity_compartment.blue.id
-  defined_tags = {
-    "Oracle-Tags.CreatedBy" = "default/lorentz0021@gmail.com"
-    "Oracle-Tags.CreatedOn" = "2023-06-17T13:44:33.204Z"
-  }
-  display_name      = "green"
-  extended_metadata = {}
-  fault_domain      = "FAULT-DOMAIN-3"
-  freeform_tags     = {}
-
-  ipxe_script                         = ""
-  is_pv_encryption_in_transit_enabled = false
-
-  preserve_boot_volume = false
-  shape                = data.oci_core_images.ubuntu_minimal.shape
-
-  state = "RUNNING"
-
-  agent_config {
-    are_all_plugins_disabled = false
-    is_management_disabled   = false
-    is_monitoring_disabled   = false
-  }
-
-  availability_config {
-    is_live_migration_preferred = false
-    recovery_action             = "RESTORE_INSTANCE"
-  }
-
-  create_vnic_details {
-    subnet_id = oci_core_subnet.blue.id
-  }
-
-  instance_options {
-    are_legacy_imds_endpoints_disabled = false
-  }
-
-  launch_options {
-    boot_volume_type                    = "PARAVIRTUALIZED"
-    firmware                            = "UEFI_64"
-    is_consistent_volume_naming_enabled = true
-    network_type                        = "PARAVIRTUALIZED"
-    remote_data_volume_type             = "PARAVIRTUALIZED"
-  }
-
-  shape_config {
-    ocpus         = 1
-    memory_in_gbs = 1
-  }
-
-  source_details {
-    source_id   = data.oci_core_images.ubuntu_minimal.images[0].id
-    source_type = "image"
   }
 }
 
