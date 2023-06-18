2624305a11b348ab2a37c17d5d10ce67d1ce1464
lens0021 <lorentz0021@gmail.com>
2023-06-18 16:41:51 +0900

    Install docker

--- oci/core_instance_user_data.sh
+++ oci/core_instance_user_data.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Install and configure Docker
+# https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+sudo apt-get update
+sudo apt-get install -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+systemctl enable docker
+systemctl start docker
+sudo usermod -a -G docker ubuntu
 
 echo Hello, OCI
