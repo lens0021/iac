94eb8bca972c33face8c5a8f1435c7ce99758eeb
lens0021 <lorentz0021@gmail.com>
2024-01-20 00:41:51 +0900

    vnic

--- oci/core_instances.tf
+++ oci/core_instances.tf
@@ -5,6 +5,15 @@ resource "oci_core_instance" "blue" {
   availability_domain = data.oci_identity_availability_domain.ad1.name
   shape               = data.oci_core_images.ubuntu_minimal.shape
 
+  metadata = {
+    user_data = base64encode(file("core_instance_user_data.sh"))
+  }
+
+  freeform_tags = {
+    publicIP = resource.oci_core_public_ip.blue.id
+    SubnetId = oci_core_subnet.blue.id
+  }
+
   shape_config {
     ocpus         = 1
     memory_in_gbs = 1
@@ -14,21 +23,6 @@ resource "oci_core_instance" "blue" {
     source_type             = "image"
     source_id               = data.oci_core_images.ubuntu_minimal.images[0].id
     boot_volume_vpus_per_gb = "10"
-  }
-
-  create_vnic_details {
-    # assign_public_ip = true
-    private_ip = oci_core_private_ip.blue.ip_address
-    subnet_id  = oci_core_subnet.blue.id
-  }
-
-  metadata = {
-    user_data = base64encode(file("core_instance_user_data.sh"))
-  }
-
-  freeform_tags = {
-    publicIP = resource.oci_core_public_ip.blue.id
-    SubnetId = oci_core_subnet.blue.id
   }
 
   lifecycle {
--- oci/core_private_ips.tf
+++ oci/core_private_ips.tf
@@ -1,3 +1,3 @@
 resource "oci_core_private_ip" "blue" {
-  subnet_id = oci_core_subnet.blue.id
+  vnic_id = oci_core_vnic_attachment.blue
 }
