dca891526bed8ad2105349cbeb62ade2733d0b99
lens0021 <lorentz0021@gmail.com>
2024-01-20 01:12:31 +0900

    Add data.oci_core_vnic.blue

--- oci/core_vnics.tf
+++ oci/core_vnics.tf
@@ -4,3 +4,7 @@ data "oci_core_vnic_attachments" "blue" {
   availability_domain = data.oci_identity_availability_domain.ad1.name
   instance_id         = oci_core_instance.blue.id
 }
+
+data "oci_core_vnic" "blue" {
+  vnic_id = data.oci_core_vnic_attachments.blue.vnic_attachments[0].vnic_id
+}
