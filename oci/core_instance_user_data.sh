766514368c773943da7499f8526eefd1fe5c4ad8
lens0021 <lorentz0021@gmail.com>
2025-03-23 15:16:48 +0900

    Disable docker installation and prepare minecraft user

--- oci/core_instance_user_data.sh
+++ oci/core_instance_user_data.sh
@@ -18,18 +18,23 @@ apt-get install -y \
 
 # Install and configure Docker
 # https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
-apt-get install -y ca-certificates curl gnupg
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
-  tee /etc/apt/sources.list.d/docker.list >/dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
-systemctl enable docker
-systemctl start docker
-usermod -a -G docker ubuntu
+# apt-get install -y ca-certificates curl gnupg
+# install -m 0755 -d /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# chmod a+r /etc/apt/keyrings/docker.gpg
+# echo \
+#   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+#   tee /etc/apt/sources.list.d/docker.list >/dev/null
+# apt-get update
+# apt-get install -y docker-ce docker-ce-cli containerd.io
+# systemctl enable docker
+# systemctl start docker
+# usermod -a -G docker ubuntu
+
+sudo adduser --system --home /opt/minecraft minecraft
+sudo groupadd minecraft
+sudo adduser minecraft minecraft
+sudo chown -R minecraft:minecraft /opt/minecraft
 
 echo Hello, OCI
